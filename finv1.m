257a99cb802c968700b5e23963a3597facccf7cf
Uğur Dar <37039994+ugurdar@users.noreply.github.com>
2020-04-27 21:40:24 +0300

    Update finv1.m

--- finv1.m
+++ finv1.m
@@ -1,6 +1,1 @@
-function p = fcdf1(x,v1,v2)
-syms t
-f1 = gamma((v1+v2)/2)/(gamma(v1/2)*gamma(v2/2))*(v1/v2)^(v1/2)*t^((v1-2)/2)/((1+(v1/v2)*t)^((v1+v2)/2));
-sonuc = int(f1,0,x);
-sonuc = double(sonuc);
-p = sonuc;
+
